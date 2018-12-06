15b23150137796290c7c811aedcbed650ee2b676
perlancar (on pc-office) <perlancar@gmail.com>
2018-12-06 15:16:29 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/DataCmp.pm
@@ -0,0 +1,68 @@
+package Bencher::Scenario::DataCmp;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+$main::data1M = join("", map {chr(256*rand)} 1..(1024*1024));
+
+our $scenario = {
+    summary => 'Benchmark Data::Cmp against similar solutions',
+    participants => [
+        {
+            fcall_template => 'Data::Cmp::cmp_data(<data1>, <data2>)',
+        },
+        {
+            fcall_template => 'Data::Cmp::Numeric::cmp_data(<data1>, <data2>)',
+        },
+        {
+            fcall_template => 'Data::Cmp::StrOrNumeric::cmp_data(<data1>, <data2>)',
+        },
+        {
+            module => 'JSON::PP',
+            code_template => 'JSON::PP::encode_json(<data1>) eq JSON::PP::encode_json(<data2>)',
+        },
+        {
+            fcall_template => 'Data::Compare::Compare(<data1>, <data2>)',
+        },
+    ],
+
+    datasets => [
+        {
+            name=>'empty arrays',
+            args=>{
+                data1=>[],
+                data2=>[],
+            },
+        },
+        {
+            name=>'small arrays',
+            args=>{
+                data1=>[1,2,[],3,4],
+                data2=>[1,2,[],5,4],
+            },
+        },
+        {
+            name=>'1k array of ints',
+            args=>{
+                data1=>[1..1000],
+                data2=>[1..1000],
+            },
+        },
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 BENCHMARK NOTES
+
+
+=head1 SEE ALSO
+
+L<Bencher::Scenario::ScalarCmp>
+
+L<Bencher::Scenario::Serializers>
